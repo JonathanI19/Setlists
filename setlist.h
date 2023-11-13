4142069f38939dfef74b61237d4000ad6fe096e3
Jonathan Isely <63868039+JonathanI19@users.noreply.github.com>
2023-11-13 16:19:40 -0500

    Update setlist.h

--- setlist.h
+++ setlist.h
@@ -2,11 +2,8 @@
 #define SETLIST
 
 void HarryPotter(int buzzer);
-void Asabranca(int buzzer);
 void CantinaBand(int buzzer);
 void Pacman(int buzzer);
-void GameOfThrones(int buzzer);
 void TakeOnMe(int buzzer);
-void NeverGonna(int buzzer);
 
 #endif
